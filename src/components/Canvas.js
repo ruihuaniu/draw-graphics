589a65e2493d34a5578f06c40a58c50b05aa67c4
Barton NIU <barton.niu@gmail.com>
2020-05-15 11:00:32 +1000

    Update  the data structure of colors

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -5,27 +5,41 @@ function Canvas() {
     const canvasRef = useRef(null)
 
 
-    // form 32,768 discrete colors
-    const colors = []
-    for (let x = 1; x <= 32; x++) {
-        for (let y = 1; y <= 32; y++) {
-            for (let z = 1; z <= 32; z++) {
-                colors.push(`rgb(${x * 8},${y * 8},${z * 8})`)
-            }
-        }
-    }
 
-    console.log(colors.length);
 
 
     useEffect(() => {
+        // form 32,768 discrete colors
+        const colors = []
+        let subColors = []
+        for (let x = 1; x <= 32; x++) {
+            for (let y = 1; y <= 32; y++) {
+                for (let z = 1; z <= 32; z++) {
+                    subColors.push(`rgb(${x * 8},${y * 8},${z * 8})`)
+                    if (y % 4 === 0 && z === 32) {
+                        colors.push(subColors)
+
+                        subColors = [];
+
+                    }
+                }
+            }
+        }
+
+        console.log(colors[0][0]);
+
+
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d')
 
-        for (let i = 1; i <= 256; i++) {
-            for (let j = 1; j <= 128; j++) {
-                ctx.fillStyle = colors[i * j]
-                ctx.fillRect(i - 1, j - 1, 1, 1)
+        for (let i = 0; i < 256; i++) {
+            console.log(colors[i]);
+
+            for (let j = 0; j < 128; j++) {
+
+
+                ctx.fillStyle = colors[i][j]
+                ctx.fillRect(i, j, 1, 1)
             }
         }
 
