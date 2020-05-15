ea1deda7d3990db85469a0efa1f69fd9999e7213
Barton NIU <barton.niu@gmail.com>
2020-05-15 21:37:34 +1000

    Update code structure

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -5,55 +5,68 @@ function Canvas() {
     const canvasRef = useRef(null)
 
     useEffect(() => {
-        // form 32,768 discrete colors
-        const colors = []
-        let subColors = []
-        for (let x = 1; x <= 32; x++) {
-            for (let y = 1; y <= 32; y++) {
-                for (let z = 1; z <= 32; z++) {
-                    subColors.push(`rgb(${x * 8},${y * 8},${z * 8})`)
-                    if (y % 4 === 0 && z === 32) { // if there are 128 colors in the subColor array, then push it to the main colors array 
-                        colors.push(subColors)
-                        subColors = [];
+
+        // generate 32,768 discrete colors
+        function generateColors(steps, canvasHeight) {
+            const colors = []
+            let subColors = []
+            for (let x = 1; x <= steps; x++) {
+                for (let y = 1; y <= steps; y++) {
+                    for (let z = 1; z <= steps; z++) {
+                        subColors.push(`rgb(${x * 256 / steps},${y * 256 / steps},${z * 256 / steps})`)
+                        if (y % (canvasHeight / steps) === 0 && z === steps) { // check if there are 128 colors in the subColor array, if so, push it to the main colors array 
+                            colors.push(subColors)
+                            subColors = [];
+                        }
                     }
+                }
+            }
+
+            return colors
+
+        }
+
+        // shuffle colors 
+        function shuffleColors(colors) {
+
+            for (let i = 1; i < colors.length; i++) {
+                for (let x = 1; x < colors[i].length; x++) {  //shuffle subColors
+                    const y = Math.floor(Math.random() * (x + 1));
+                    [colors[i][x], colors[i][y]] = [colors[i][y], colors[i][x]];
+                }
+                const j = Math.floor(Math.random() * (i + 1));
+                [colors[i], colors[j]] = [colors[j], colors[i]];
+            }
+
+            console.log(colors);
+            return colors;
+        }
+
+        // draw canvas
+        function drawCanvas(colors, canvasWidth, canvasHeight) {
+            const canvas = canvasRef.current;
+            const ctx = canvas.getContext('2d')
+
+            for (let i = 0; i < canvasWidth; i++) {
+                for (let j = 0; j < canvasHeight; j++) {
+                    ctx.fillStyle = colors[i][j];
+                    ctx.fillRect(i, j, 1, 1)
                 }
             }
         }
 
-        // shuffle colors
-        for (let i = colors.length - 1; i >= 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [colors[i], colors[j]] = [colors[j], colors[i]];
-        }
+        drawCanvas(shuffleColors(generateColors(32, 128)), 256, 128) // colors with 32 steps. canvas width is 256px, height is 128px
 
-        console.log(colors[0][0]);
-
-
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d')
-
-        for (let i = 0; i < 256; i++) {
-            // console.log("i is", i, colors[i]);
-            for (let j = 0; j < 128; j++) {
-                ctx.fillStyle = colors[i][j];
-                ctx.fillRect(i, j, 1, 1)
-            }
-        }
-
-        // ctx.fillStyle = 'red'
-        // ctx.fillRect(100, 100, 450, 400)
     }, [])
 
 
     return (
         <div className="container">
+
             <canvas
                 ref={canvasRef}
-            // width="256px"
-            // height="128px"
-            // className="canvas container"
             />
-            this is canvas page
+
         </div>
     )
 }
