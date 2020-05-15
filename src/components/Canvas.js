6e1bda99eb49114a0f5909a11f2760114050a1e5
Barton NIU <barton.niu@gmail.com>
2020-05-15 20:03:49 +1000

    Add shuffle colors function

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,10 +4,6 @@ import './Canvas.css'
 function Canvas() {
     const canvasRef = useRef(null)
 
-
-
-
-
     useEffect(() => {
         // form 32,768 discrete colors
         const colors = []
@@ -16,14 +12,18 @@ function Canvas() {
             for (let y = 1; y <= 32; y++) {
                 for (let z = 1; z <= 32; z++) {
                     subColors.push(`rgb(${x * 8},${y * 8},${z * 8})`)
-                    if (y % 4 === 0 && z === 32) {
+                    if (y % 4 === 0 && z === 32) { // if there are 128 colors in the subColor array, then push it to the main colors array 
                         colors.push(subColors)
-
                         subColors = [];
-
                     }
                 }
             }
+        }
+
+        // shuffle colors
+        for (let i = colors.length - 1; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [colors[i], colors[j]] = [colors[j], colors[i]];
         }
 
         console.log(colors[0][0]);
@@ -33,12 +33,9 @@ function Canvas() {
         const ctx = canvas.getContext('2d')
 
         for (let i = 0; i < 256; i++) {
-            console.log(colors[i]);
-
+            // console.log("i is", i, colors[i]);
             for (let j = 0; j < 128; j++) {
-
-
-                ctx.fillStyle = colors[i][j]
+                ctx.fillStyle = colors[i][j];
                 ctx.fillRect(i, j, 1, 1)
             }
         }
